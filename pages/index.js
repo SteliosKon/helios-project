d32c103eef7049a9fe2fd5d48e03974e6465cbee
stelioskon <steliosnteloss@gmail.com>
2021-07-21 13:30:47 +0300

    ...

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,29 @@
 import React from 'react'
+import Image from 'next/image'
 
 const index = () => {
   return (
-    <div
-      style={{
-        textAlign: 'center',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-      }}
-    >
-      Site Under Construction
-    </div>
+    <>
+      <div style={{ margin: '10px', position: 'absolute' }}>
+        <Image src='/logo.jpg' alt='Logo' width={50} height={50} />
+      </div>
+
+      <div
+        style={{
+          textAlign: 'center',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          flexDirection: 'column',
+          background: 'lightyellow',
+        }}
+      >
+        <h1>Coming Soon</h1>
+        <h3>Site Under Construction</h3>
+        <Image src='/logo.jpg' alt='Logo' width={50} height={50} />
+      </div>
+    </>
   )
 }
 
